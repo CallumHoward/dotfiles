8d922b94edb839f997b82e17774d459cc380eed3
CallumHoward <callumh@cse.unsw.edu.au>
2017-07-10 03:40:27 +1000

    added omz to install

--- install.sh
+++ install.sh
@@ -10,21 +10,29 @@ declare -a files=(
     #'.gvimrc'
     #'.xvimrc'
 
-    #'.zshrc'
+    '.zshrc'
     #'.oh-my-zsh'
     '.shell_aliases'
     '.shell_exports'
-    #'.zsh_keybindings'
-    #'.zsh_styles'
-    #'.zsh_functions'
+    '.zsh_keybindings'
+    '.zsh_styles'
+    '.zsh_functions'
 )
 
-if [ "${PWD##*/}" == "dotfiles" ]; then
-    for i in "${files[@]}"; do
-        if [ "`ls ~/$i 2> /dev/null`" == "" ]; then
-            ln -sv "`pwd`/$i" ~/"$i"
-        fi
-    done
-else
+if [ "${PWD##*/}" != "dotfiles" ]; then
     echo "$0: run this script from inside the dotfiles directory"
+    exit
+fi
+
+for i in "${files[@]}"; do
+    if [ "`ls ~/$i 2> /dev/null`" == "" ]; then
+        ln -sv "`pwd`/$i" ~/"$i"
+    fi
+done
+
+echo -n "Install oh-my-zsh [Y/n]? "
+read answer
+if echo "$answer" | grep -viq "^n" ;then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    ln -fs `pwd`/.oh-my-zsh/custom ~/.oh-my-zsh/custom
 fi
