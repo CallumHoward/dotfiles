40014487c82d9fca732d342f336f7eda5bdb9c5d
CallumHoward <callumh@cse.unsw.edu.au>
2018-10-14 03:49:41 +1100

    oni conifg

---
+++ .config/oni/config.js
@@ -0,0 +1,31 @@
+"use strict";
+exports.__esModule = true;
+exports.activate = function (oni) {
+    console.log("config activated");
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+};
+exports.deactivate = function (oni) {
+    console.log("config deactivated");
+};
+exports.configuration = {
+    //add custom config here, such as
+    "ui.colorscheme": "nord",
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    //"editor.fontSize": "12px",
+    //"editor.fontFamily": "Monaco",
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "sidebar.enabled": false,
+    "editor.fullScreenOnStart": true
+};
