208fe366ac5372c65e486985df9547bdd8bde9a3
CallumHoward <callumh@cse.unsw.edu.au>
2019-04-30 23:57:17 +1000

    updated oni config

--- .config/oni/config.tsx
+++ .config/oni/config.tsx
@@ -1,41 +1,39 @@
-
 import * as React from "react"
 import * as Oni from "oni-api"
 
 export const activate = (oni: Oni.Plugin.Api) => {
-    console.log("config activated")
+  console.log("config activated")
 
-    // Input
-    //
-    // Add input bindings here:
-    //
-    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+  // Input
+  //
+  // Add input bindings here:
+  //
+  oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
 
-    //
-    // Or remove the default bindings here by uncommenting the below line:
-    //
-    // oni.input.unbind("<c-p>")
-
+  //
+  // Or remove the default bindings here by uncommenting the below line:
+  //
+  // oni.input.unbind("<c-p>")
 }
 
 export const deactivate = (oni: Oni.Plugin.Api) => {
-    console.log("config deactivated")
+  console.log("config deactivated")
 }
 
 export const configuration = {
-    //add custom config here, such as
+  //add custom config here, such as
 
-    "ui.colorscheme": "nord",
+  "ui.colorscheme": "nord",
 
-    //"oni.useDefaultConfig": true,
-    //"oni.bookmarks": ["~/Documents"],
-    //"oni.loadInitVim": false,
-    //"editor.fontSize": "12px",
-    //"editor.fontFamily": "Monaco",
+  //"oni.useDefaultConfig": true,
+  //"oni.bookmarks": ["~/Documents"],
+  //"oni.loadInitVim": false,
+  //"editor.fontSize": "12px",
+  //"editor.fontFamily": "Monaco",
 
-    // UI customizations
-    "ui.animations.enabled": true,
-    "ui.fontSmoothing": "auto",
-    "sidebar.enabled": false,
-    "editor.fullScreenOnStart": true,
+  // UI customizations
+  "ui.animations.enabled": true,
+  "ui.fontSmoothing": "auto",
+  "sidebar.enabled": false,
+  "editor.fullScreenOnStart": true,
 }
