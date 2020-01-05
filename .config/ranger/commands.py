8a6d6b6638ef428825d1f2f79319c85f5d6825c0
CallumHoward <callumh@cse.unsw.edu.au>
2020-01-05 23:07:45 +1100

    ranger select file fzf command

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1827,6 +1827,29 @@ class fzf_fasd(Command):
             else:
                 self.fm.select_file(fzf_file)
 
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        # match files and directories
+        command="fzf +m"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
 
 # Fasd with ranger (Command Line Style)
 # https://github.com/ranger/ranger/wiki/Commands
