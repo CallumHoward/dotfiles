5b6399a50b7ae1160095c5eaf7eb6122c15aa221
CallumHoward <callum.howard@safetyculture.io>
2021-10-04 23:16:55 +1100

    Use 0.5 compat branch for treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -45,7 +45,7 @@ return require("packer").startup(function(use)
   use("kevinhwang91/nvim-bqf")
 
   -- Language features
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", branch = "0.5-compat" })
   use("nvim-treesitter/playground")
 
   -- LSP features
