411c1ac1bab062f1cc92914de1cb4c055193d27a
CallumHoward <callum.howard@safetyculture.io>
2022-04-17 20:02:24 +1000

    Update cmp config

--- .config/nvim/lua/cmp_config.lua
+++ .config/nvim/lua/cmp_config.lua
@@ -19,7 +19,6 @@ cmp.setup({
   formatting = {
     format = lspkind.cmp_format({
       preset = "codicons",
-      with_text = true,
       maxwidth = 50,
       menu = {
         buffer = "buff",
@@ -60,7 +59,7 @@ cmp.setup({
 
     -- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
     -- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    -- ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
