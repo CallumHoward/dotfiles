5c583beb1d2713cbe11c030f137d1bcb0d7b1036
CallumHoward <callum.howard@safetyculture.io>
2022-05-02 01:01:43 +1000

    Convert mappings for package-info to new syntax

--- .config/nvim/lua/plugin_config.lua
+++ .config/nvim/lua/plugin_config.lua
@@ -286,51 +286,20 @@ vim.api.nvim_exec(
 )
 
 -- Package info config
-require("package-info").setup({
+local pi = require("package-info")
+pi.setup({
   colors = {
     up_to_date = "#565f89", -- Text color for up to date package virtual text
     outdated = "#e0af68", -- Text color for outdated package virtual text
   },
 })
--- Show package versions
-vim.api.nvim_set_keymap("n", "<leader>ns", ":lua require('package-info').show()<CR>", { silent = true, noremap = true })
--- Hide package versions
-vim.api.nvim_set_keymap("n", "<leader>nc", ":lua require('package-info').hide()<CR>", { silent = true, noremap = true })
--- Update package on line
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>nu",
-  ":lua require('package-info').update()<CR>",
-  { silent = true, noremap = true }
-)
--- Delete package on line
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>nd",
-  ":lua require('package-info').delete()<CR>",
-  { silent = true, noremap = true }
-)
--- Install a new package
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>ni",
-  ":lua require('package-info').install()<CR>",
-  { silent = true, noremap = true }
-)
--- Reinstall dependencies
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>nr",
-  ":lua require('package-info').reinstall()<CR>",
-  { silent = true, noremap = true }
-)
--- Install a different package version
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>np",
-  ":lua require('package-info').changeversion()<CR>",
-  { silent = true, noremap = true }
-)
+vim.keymap.set("n", "<leader>ns", pi.show) -- Show package versions
+vim.keymap.set("n", "<leader>nc", pi.hide) -- Hide package versions
+vim.keymap.set("n", "<leader>nu", pi.update) -- Update package on line
+vim.keymap.set("n", "<leader>nd", pi.delete) -- Delete package on line
+vim.keymap.set("n", "<leader>ni", pi.install) -- Install a new package
+vim.keymap.set("n", "<leader>nr", pi.reinstall) -- Reinstall dependencies
+vim.keymap.set("n", "<leader>np", pi.changeversion) -- Install a different package version
 
 -- Debug
 -- require("dapui").setup()
