5d517e8b385f3d5df33b9f088f3aaac95aa0aef9
CallumHoward <callum.howard@safetyculture.io>
2022-10-02 14:28:20 +1100

    Add termsupport which apparently wasn't committed

---
+++ .oh-my-zsh/custom/termsupport.zsh
@@ -0,0 +1,44 @@
+# usage: title short_tab_title [long_window_title]
+#
+# See: http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
+# Fully supports screen, iterm, and probably most modern xterm and rxvt
+# (In screen, only short_tab_title is used)
+# Limited support for Apple Terminal (Terminal can't set window and tab separately)
+function title {
+  emulate -L zsh
+  setopt prompt_subst
+
+  [[ "$INSIDE_EMACS" == *term* ]] && return
+
+  # if $2 is unset use $1 as default
+  # if it is set and empty, leave it as is
+  : ${2=$1}
+
+  case "$TERM" in
+    cygwin|xterm*|putty*|rxvt*|konsole*|ansi|mlterm*|alacritty|st*)
+      if [[ "$TERM_PROGRAM" == "tmux" ]]; then
+        print -Pn "\ek${1:q}\e\\" # set screen hardstatus
+      fi
+      print -Pn "\e]2;${2:q}\a" # set window name
+      print -Pn "\e]1;${1:q}\a" # set tab name
+      ;;
+    screen*|tmux*)
+      print -Pn "\ek${1:q}\e\\" # set screen hardstatus
+      ;;
+    *)
+      if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+        print -Pn "\e]2;${2:q}\a" # set window name
+        print -Pn "\e]1;${1:q}\a" # set tab name
+      else
+        # Try to use terminfo to set the title
+        # If the feature is available set title
+        if [[ -n "$terminfo[fsl]" ]] && [[ -n "$terminfo[tsl]" ]]; then
+          echoti tsl
+          print -Pn "$1"
+          echoti fsl
+        fi
+      fi
+      ;;
+  esac
+}
+
