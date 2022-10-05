75ac0f0c6a724f72122e79bdc1ab3a11fc866c84
CallumHoward <callum.howard@safetyculture.io>
2022-10-06 02:59:31 +1100

    Add cmp commandline mappings

--- .config/nvim/lua/plugins/cmp_config.lua
+++ .config/nvim/lua/plugins/cmp_config.lua
@@ -64,17 +64,16 @@ cmp.setup({
     -- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
+    ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+    ["<C-f>"] = cmp.mapping({ c = cmp.mapping.close() }),
+    ["<C-d>"] = cmp.mapping({ c = cmp.mapping.close() }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
   },
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
     { name = "vsnip" },
+    { name = "nvim_lsp" },
   }, {
     { name = "nvim_lua" },
     { name = "cmp_tabnine" },
