afb2591fea1fffa8fac3059b6f96d74741b07197
CallumHoward <7144420+CallumHoward@users.noreply.github.com>
2026-04-06 10:40:09 +1000

    mini.ai fixes and new mappings

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,16 +1,30 @@
 return {
-  -- {
-  --   "echasnovski/mini.indentscope",
-  --   -- enabled = false,
-  --   config = {
-  --     draw = {
-  --       animation = require("mini.indentscope").gen_animation.none(),
-  --     },
-  --   },
-  -- },
+  {
+    "nvim-mini/mini.ai",
+    opts = function(_, opts)
+      opts.custom_textobjects = opts.custom_textobjects or {}
+      opts.custom_textobjects.L = function()
+        local line_num = vim.fn.line(".")
+        local line = vim.fn.getline(line_num)
+        local from_col = line:match("^(%s*)"):len() + 1
+        local to_col = line:len()
+        return { from = { line = line_num, col = from_col }, to = { line = line_num, col = to_col } }
+      end
+      LazyVim.on_load("which-key.nvim", function()
+        vim.schedule(function()
+          require("which-key").add({
+            mode = { "o", "x" },
+            { "aL", desc = "line" },
+            { "iL", desc = "line" },
+          })
+        end)
+      end)
+      return opts
+    end,
+  },
   {
     "nvim-mini/mini.surround",
-    keys = { "v", "S" },
+    event = "VeryLazy",
     opts = function()
       -- Remap adding surrounding to Visual mode selection
       -- vim.keymap.del("x", "ys")
@@ -25,6 +39,9 @@ return {
       vim.keymap.set("n", "yss", "ys_", { remap = true, desc = "Add surrounding" })
 
       return {
+        custom_surroundings = {
+          B = { input = { "%b{}", "^.().*().$" }, output = { left = "{", right = "}" } },
+        },
         mappings = {
           add = "ys", -- Add surrounding in Normal and Visual modes
           delete = "ds", -- Delete surrounding
